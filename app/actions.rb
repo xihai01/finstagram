de71ca9bb697e19caac72893b4e07fd0a08a9c0b
xihai01 <xihai.luo01@gmail.com>
2021-07-17 22:36:17 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -11,7 +11,7 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-    finstagram_post_shark = {
+    @finstagram_post_shark = {
         username: "sharky_j",
         avatar_url: "http://naserca.com/images/sharky_j.jpg",
         photo_url: "http://naserca.com/images/shark.jpg",
@@ -24,7 +24,7 @@ get '/' do
         }]
     }
 
-    finstagram_post_whale = {
+    @finstagram_post_whale = {
         username: "kirk_whalum",
         avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
         photo_url: "http://naserca.com/images/whale.jpg",
@@ -37,7 +37,7 @@ get '/' do
         }]
     }
 
-    finstagram_post_marlin = {
+    @finstagram_post_marlin = {
         username: "marlin_peppa",
         avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
         photo_url: "http://naserca.com/images/marlin.jpg",
@@ -63,5 +63,9 @@ get '/' do
         #return this instead
      #   "less than an hour"
     #end
-    [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+   #[finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+
+    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+    erb(:index)
 end
