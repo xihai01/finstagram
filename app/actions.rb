507d282be1c0eb3843960870cc6fce3252cff1a4
xihai01 <xihai.luo01@gmail.com>
2021-07-31 21:39:54 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -2,9 +2,41 @@
 #    File.read(File.join('app/views', 'index.html'))
 # end
 
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    #Find user by username
+    user = User.find_by(username: username)
+
+    #Check if the user exists
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
 
 get '/signup' do
@@ -25,7 +57,7 @@ post '/signup' do
     # if user validation pass and user is saved
     if @user.save
         # return readable representation of User objects
-        "User #{username} saved!"
+        redirect to('/login')
     else
         # try again
         erb(:signup)
