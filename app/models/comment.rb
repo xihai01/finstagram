af66b00e47c57ffa5293bae7e7545a74d2a26020
xihai01 <xihai.luo01@gmail.com>
2021-08-07 22:27:28 +0000

    W6D1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :finstagram_post
 
+    # New validation code
+    validates_presence_of :text, :user, :finstagram_post
+
 end
